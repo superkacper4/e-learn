a919fd446aec70b59aa1f509dd082fdb3ab81b02
Kacper <kucza06@wp.pl>
2020-04-06 16:06:46 +0200

    add styled components, .env and add global style

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+*{
+    box-sizing:border-box;
+}
+
+html{
+    font-size: 62.5%;
+}
+
+body {
+    margin: 0;
+    padding:0;
+    font-family: 'Montserrat', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+`;
+
+export default GlobalStyle;
