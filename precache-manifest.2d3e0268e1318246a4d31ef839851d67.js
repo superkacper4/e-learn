69106d1009bf116da22669e3b76f5954e925ecd5
Kacper <kucza06@wp.pl>
2020-06-21 21:57:26 +0200

    Updates

---
+++ precache-manifest.2d3e0268e1318246a4d31ef839851d67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94da8d4eaffbf729d9f7825399bc9982",
+    "url": "/e-learn/index.html"
+  },
+  {
+    "revision": "4e2e5d3055b2258b5f77",
+    "url": "/e-learn/static/js/2.0c0b17db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-learn/static/js/2.0c0b17db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcc68d313991a0f6048",
+    "url": "/e-learn/static/js/main.bb99adb5.chunk.js"
+  },
+  {
+    "revision": "f66080ad562d478f2add",
+    "url": "/e-learn/static/js/runtime-main.7f725bd9.js"
+  },
+  {
+    "revision": "855be58f04586da9c4d574fbdde89a6f",
+    "url": "/e-learn/static/media/me.855be58f.png"
+  },
+  {
+    "revision": "8959d7d38af5e63ded0ab6e9c7263e74",
+    "url": "/e-learn/static/media/ornottobe.8959d7d3.jpg"
+  },
+  {
+    "revision": "3c5954bd327cb88fa4ff18f83e4c0ca1",
+    "url": "/e-learn/static/media/pastsimple.3c5954bd.jpg"
+  },
+  {
+    "revision": "2b9e8cd03ee47aa20b97e4c6310e5e23",
+    "url": "/e-learn/static/media/presentsimple.2b9e8cd0.jpg"
+  },
+  {
+    "revision": "8acd815cfde241cd47981eab98af200a",
+    "url": "/e-learn/static/media/start.8acd815c.jpg"
+  },
+  {
+    "revision": "7f893ce4392c48e0ad0b2a9b68e2ed6b",
+    "url": "/e-learn/static/media/tobe.7f893ce4.jpg"
+  }
+]);
